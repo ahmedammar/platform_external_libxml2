824e7ea8ec696d2009372fe0778f200d66b899bb
Ahmed Ammar <aammar@genesi-usa.com>
2011-09-20 16:41:16 +0200

    Build a shared library too.
    Change-Id: I5948ed9595bfee7add96d588d00b1af31248e91d

--- Android.mk
+++ Android.mk
@@ -65,6 +65,18 @@ LOCAL_MODULE:= libxml2
 
 include $(BUILD_STATIC_LIBRARY)
 
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(common_SRC_FILES)
+LOCAL_C_INCLUDES += $(common_C_INCLUDES) external/icu4c/common
+LOCAL_SHARED_LIBRARIES += $(common_SHARED_LIBRARIES) libicuuc
+#LOCAL_CFLAGS += -fvisibility=hidden
+
+LOCAL_MODULE:= libxml2
+
+LOCAL_PRELINK_MODULE := false
+
+include $(BUILD_SHARED_LIBRARY)
 
 # For the host
 # ========================================================
